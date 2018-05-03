ff27f4d1b963c32415d3400b3df3ed7e7f25d332
leeY1ng <869500351@qq.com>
2018-05-03 16:03:00 +0800

    webpack

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import _ from "lodash";
+import printMe from "./print.js";
+
+function component() {
+	var element = document.createElement("div");
+	var btn = document.createElement("button");
+
+	element.innerHTML =  _.join(["Hello","Webpack"], " ");
+	
+	btn.innerHTML = "Click me and check console";
+	btn.onclick = printMe;
+
+	element.appendChild(btn);
+
+	return element;
+}
+document.body.appendChild(component());
